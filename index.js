a909640d23c1a9442613f9e4d0bdab5582073282
Peter Forsgren Christensen <peter.f.chris@gmail.com>
2019-05-16 12:25:37 -0600

    step 3 done

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ require('dotenv').config()
 const express = require("express");
 const massive = require('massive')
 const app = express();
-
-
-const { SERVER_PORT } = process.env;
+const {SERVER_PORT, CONNECTION_STRING} = process.env;
 
 app.use(express.json());
 
-app.listen(SERVER_PORT, () => {
+massive(CONNECTION_STRING).then((dbInstance) => {
+  app.set('db', dbInstance)
+  console.log('database set!')
   console.log(`Server listening on port ${SERVER_PORT}`);
+})
+
+app.listen(SERVER_PORT, () => {
 });
