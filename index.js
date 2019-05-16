036c1b6d3bbf22280971b99f4c27052e5bb47b63
Peter Forsgren Christensen <peter.f.chris@gmail.com>
2019-05-16 12:33:13 -0600

    step 4 done

--- index.js
+++ index.js
@@ -6,11 +6,19 @@ const {SERVER_PORT, CONNECTION_STRING} = process.env;
 
 app.use(express.json());
 
-massive(CONNECTION_STRING).then((dbInstance) => {
-  app.set('db', dbInstance)
-  console.log('database set!')
-  console.log(`Server listening on port ${SERVER_PORT}`);
+massive(CONNECTION_STRING)
+  .then((dbInstance) => {
+    app.set('db', dbInstance)
+    console.log('database set!')
+  // dbInstance.new_planes()
+  // .then((planes) => {
+  //   console.log(planes)
+  // .catch((err) => {
+  //   console.log(err)
+  // }) 
+  // })
+  app.listen(SERVER_PORT, () => {
+    console.log(`Server listening on port ${SERVER_PORT}`);
+  });
 })
 
-app.listen(SERVER_PORT, () => {
-});
